1f15dd8121206cc95b11045d315fbad7e7eff4c0
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-11-29 17:17:59 +0000

    basic css and html

--- src/handlers.js
+++ src/handlers.js
@@ -38,10 +38,10 @@ const handlePublic = (request, response, url) => {
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(404, "Content-Type: text/html");
+      response.writeHead(404, { "Content-Type": "text/html" });
       response.end("<h1>File not found</h1>");
     } else {
-      response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
+      response.writeHead(200, {"Content-Type": extensionType[extension]});
       response.end(file);
     }
   });
