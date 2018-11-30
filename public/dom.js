482564d11e09b22346878aae3cf37e3a60a185a8
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-11-30 11:22:12 +0000

    nothing to do with Wikipedia, output services to DOM

--- public/dom.js
+++ public/dom.js
@@ -1,1 +1,17 @@
+// DOM manipulation here
+
 "use strict";
+
+// our callback function
+const outputServices = (response) => {
+
+    const outputItems = response.map(item => `
+    <li class="service-item">
+        <img class="service-item__image" src="${item.img}" alt="${item.title}"/>
+        <div class="service-item__title">${item.title}</div>
+        <a href="${item.url}">View on CoTech website</a>
+    </li>
+    `).join('');
+    document.getElementById("services").innerHTML = outputItems;
+};
+
